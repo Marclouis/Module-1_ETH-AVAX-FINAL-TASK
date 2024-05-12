2bb6751cdb1d418f27af873bd36777bb9f2be2db
Marclouis <137591203+Marclouis@users.noreply.github.com>
2024-05-13 01:23:47 +0800

    Update Module1.sol

--- Module1.sol
+++ Module1.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract checkOwner {
+    address public contractOwner;
+
+    constructor() {
+        contractOwner = msg.sender;
+    }
+
+    error throwError(string, address);
+
+    function checkRequire() public view returns (string memory) {
+        require(msg.sender == contractOwner, "You are not the owner");
+        return "Verified Owner";
+    }
+
+    function checkRevert() view public returns (string memory) {
+        if (msg.sender != contractOwner) {
+            revert throwError("You are not the owner", msg.sender);
+        }
+        return "Verified Owner";
+    }
+
+    function checkAssert() public view returns (string memory) {
+        assert(msg.sender == contractOwner);
+        return "Verified Owner";
+    }
+}
